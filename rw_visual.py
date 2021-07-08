1214a9cbfe886f38777426c643c6a635b0f40a3c
Ken V <ken.valverde@yahoo.com>
2021-07-08 13:56:22 -0500

    Alter the plotting window to fit the screen

--- rw_visual.py
+++ rw_visual.py
@@ -5,18 +5,21 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9))
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Purples, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Purples, edgecolors='none', s=1)
 
     # Emphasize the start and end points
     ax.scatter(0, 0, c='green', edgecolors='none', s=50)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
